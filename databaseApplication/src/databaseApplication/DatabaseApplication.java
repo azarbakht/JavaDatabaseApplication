de9cecb609c86dc1294ce255e1797e2b247a2364
Emerson Amirhosein Azarbakht <azarbaam@eecs.oregonstate.edu>
2016-01-02 12:43:16 -0800

    connection to local host successful

--- databaseApplication/src/databaseApplication/DatabaseApplication.java
+++ databaseApplication/src/databaseApplication/DatabaseApplication.java
@@ -20,15 +20,30 @@ public class DatabaseApplication {
 		
 		System.out.println("Database driver loaded");
 		
-		
 		// establish a connection to the MySQL database
-		Connection connection = DriverManager.getConnection("jdbc:mysql://azarbakht.info/swithd_JavaDatabaseApplication");
+ 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "swithd_javadba", "emerson(&)");
+//		Connection connection = DriverManager.getConnection("jdbc:mysql://www.azarbakht.info/swithd_JavaDatabaseApplication", "swithd_javadba", "emerson(&)");
 		System.out.println("Database connection established");
 		
 //		// establish a connection to the Oracle database
 //		Connection connection2 = DriverManager.getConnection("jdbc:oracle:thin:@hostname:port#:oracleDBSID");
 		
+		// create a SQL statement
+		Statement statement = connection.createStatement();
+		System.out.println("Database statement created");
 		
+//		// execute a statement (which returns nothing)
+//		statement.executeUpdate("create table Temp (col1 char(5), col2 char(5))");
+//		System.out.println("Database statement executed");
+		
+		// get results of executing a statement (which returns something)
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM Movies");
+		System.out.println("Database statement executed and loaded to resultSet");
+		
+		// process the result retrieved from the database
+		while (resultSet.next()) {
+			System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4));
+		}
 		
 	}
 
